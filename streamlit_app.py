1956909f00197f3d89d3493c17883104aac48a6d
cesport <67286129+cesport@users.noreply.github.com>
2023-08-11 17:32:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,35 +9,18 @@ import pickle
 import plotly.express as px
 import folium
 from streamlit_folium import folium_static
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+from dateutil.relativedelta import relativedelta # to add days or years
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+start_date = dt.date(year=2020,month=1,day=1)
+end_date = dt.date(year=2022,month=12,day=1)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# set tabs
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Performance", "Carriers", "Flights", "Fares", "Cargo"])
 
+# PERFORMANCE TAB =============================================================
+with tab1:
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in PEDO", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+  col1, col2 = st.columns([1, 1])
+  with open('sample.txt', 'rb') as f:
+    total, airlines, complete, bystate=pickle.load(f)
