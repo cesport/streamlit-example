337411215ab0ef8f35bb8fe2b6075c1f4e2df364
cesport <67286129+cesport@users.noreply.github.com>
2023-08-11 19:06:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,107 @@ with tab1:
 
     subtab2.write("Volume of cancellations by carrier")
     subtab2.line_chart(data=airlines)
+    subtab3.write("Volume of cancellations by destination")
+
+    #Define map
+    map = folium.Map(location=[38, -96.5], zoom_start=3, scrollWheelZoom=False, tiles='CartoDB positron')
+    choropleth = folium.Choropleth(
+        geo_data='/content/drive/MyDrive/Dashboard_dataset/us-state-boundaries.geojson',
+        data=bystate,
+        columns=('DestStateName', 'Total'),
+        key_on='feature.properties.name',
+        line_opacity=0.8,
+        highlight=True)
+    choropleth.geojson.add_to(map)
+
+    df_indexed = bystate.set_index('DestStateName')
+    for feature in choropleth.geojson.data['features']:
+      state_name = feature['properties']['name']
+      feature['properties']['cancelled'] = 'Cancelled flights: ' + '{:,}'.format(df_indexed.loc[state_name, 'Total']) if state_name in list(df_indexed.index) else ''
+    choropleth.geojson.add_child(
+        folium.features.GeoJsonTooltip(['name', 'cancelled'], labels=False))
+    with subtab3:
+      folium_static(map, width=300, height=300)
+
+  col2.subheader("Taxi time")
+  fig = px.scatter(
+      complete,
+      x="TaxiOut",
+      y="DepDelay",
+      size="Distance",
+      color="Month",
+      hover_name="Operating_Airline ",
+      log_x=True,
+      size_max=60,
+  )
+
+  col2.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+  col3, col4 = st.columns(2)
+
+  col3.subheader("Carrier presence")
+  col3.write("Placeholder text used for symmetry")
+  col3.line_chart(total)
+
+  with col4:
+    st.subheader("Volume")
+    subcol1, subcol2= st.columns([1,1])
+
+    subcol1.write("Pax volume")
+    subcol1.line_chart(total)
+    subcol1.write("Delta")
+
+    subcol2.write("Cargo volume")
+    subcol2.line_chart(total)
+    subcol2.write("Delta")
+
+  #Add description to the sidebar
+  st.sidebar.header('Description')
+  st.sidebar.info('This is the description for the performance dashboard. Lorem ipsum dolor sit amet, \
+  consectetur adipiscing elit. Ut in vestibulum nisi, et blandit neque. Vestibulum ipsum nibh, \
+  consectetur in fermentum et, ornare in justo. Proin pulvinar sagittis porta. Nullam metus orci, interdum et diam sed.', icon="ℹ️")
+
+  # Add a slider to the sidebar:
+  st.sidebar.header('Time period')
+  st.sidebar.slider(
+  label='Please select an interval:',
+  min_value=start_date,
+  max_value=end_date,
+  value=(start_date, end_date),
+  format='MMM YY',
+  step=dt.timedelta(days=1)
+  )
+
+  # Add a selectbox to the sidebar:
+  st.sidebar.header('Origin airport selection')
+  st.sidebar.selectbox(
+      label='Focus:',
+      options=('CDG', 'JFK', 'NRT')
+  )
+
+  #Add radio buttons
+  st.sidebar.write('Benchmarks:')
+  st.sidebar.checkbox('CDG')
+  st.sidebar.checkbox('JFK')
+  st.sidebar.checkbox('NRT')
+
+  st.sidebar.header('Data source details')
+  st.sidebar.write('These are the data source details. Lorem ipsum dolor sit amet, \
+  consectetur adipiscing elit. Ut in vestibulum nisi, et blandit neque. Vestibulum ipsum nibh, \
+  consectetur in fermentum et, ornare in justo. Proin pulvinar sagittis porta. Nullam metus orci, interdum et diam sed.')
+
+with tab2:
+   st.header("Carriers dashboard")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("Flights dashboard")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+with tab4:
+   st.header("Fares dashboard")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+with tab5:
+   st.header("Cargo dashboard")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
