8aaff90526e630a4d12b9584172a2682fcfc4d42
cesport <67286129+cesport@users.noreply.github.com>
2023-08-16 11:41:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,6 +81,7 @@ with tab1:
     new_bystate=new_bystate[new_bystate.Month<=timerange[1].month]
     new_bystate=new_bystate.groupby(['DestStateName'])['Total'].agg('sum')
     new_bystate=pd.DataFrame(new_bystate)
+    new_bystate.reset_indes(inplace=True)
     #Define map
     map = folium.Map(location=[38, -96.5], zoom_start=3, scrollWheelZoom=False, tiles='CartoDB positron')
     choropleth = folium.Choropleth(
